30306c41df8dcce3f50009d3ffcca7d4cd61cfda
tzabar <tzabar89@gmail.com>
2018-08-05 23:37:31 +0300

    update the motors code(arduino and python)

--- motors/motors.ino
+++ motors/motors.ino
@@ -1,41 +1,27 @@
 #include <Servo.h>
 
-Servo x_servo;  // create servo object to control a servo
-Servo y_servo;  // create servo object to control a servo
+Servo x_servo;          // create servo object to control a servo
+Servo y_servo;          // create servo object to control a servo
 
 void setup() {
-  x_servo.attach(6);  // attaches the servo on pin 9 to the servo object
-  y_servo.attach(9);  // attaches the servo on pin 9 to the servo object
+  x_servo.attach(6);    // attaches the servo on pin 9 to the servo object
+  y_servo.attach(9);    // attaches the servo on pin 9 to the servo object
   
+  Serial.begin(9600);   // begin serial communication at 9600 bits of data per second between the board and the computer
 
-  Serial.begin(9600); // begin serial communication at 9600 bits of data per second between the board and the computer
-
-  x_servo.write(90);                  // sets the servo position according to the scaled value
-  y_servo.write(0);                   // sets the servo position according to the scaled value
-  Serial.println(5);
+  x_servo.write(90);    // sets the servo position according to the scaled value
+  y_servo.write(10);    // sets the servo position according to the scaled value
 }
 
 void loop() {
-  Serial.println(5);
-  delay(1000);                           // waits for the servo to get there
-  
   while (Serial.available() > 0) {
     int x_axis = Serial.parseFloat();   // convert from string to float number
-    int y_axis = Serial.parseFloat();
+    int y_axis = Serial.parseFloat();   // convert from string to float number
     
     Serial.print(x_axis);
     Serial.println(y_axis);
 
-    x_servo.write(x_axis);                  // sets the servo position according to the scaled value
-    y_servo.write(y_axis);                  // sets the servo position according to the scaled value
-    delay(15);                           // waits for the servo to get there
-
-    // look for the newline. That's the end of your sentence:
-    if (Serial.read() == '\n') {
-      Serial.print(x_axis);
-      Serial.println(y_axis);
-      
-    }
+    x_servo.write(x_axis);              // sets the servo position according to the scaled value
+    y_servo.write(y_axis);              // sets the servo position according to the scaled value
   }
 }
-
---
+++ motors/motors.py
@@ -0,0 +1,9 @@
+import serial
+import struct
+
+data = serial.Serial('com5', 9600, timeout=1)       # Start the serial port to communicate with arduino
+
+while (True):                                       # infinite while loop to keep the program running
+    x_axis = input("Enter x axis: ")                # Prompt the user for x angle
+    y_axis = input("Enter y axis: ")                # Prompt the user for y angle
+    data.write((x_axis + ',' + y_axis).encode())    # send the angle to the Arduino through serial port
