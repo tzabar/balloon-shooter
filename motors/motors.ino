c0f8cc5e8e3d5d229f89393aa39c1c199baf3453
tzabar <tzabar89@gmail.com>
2018-08-09 18:17:58 +0300

    upgrade to serial speed 115200 + fix bug

--- motors/motors.ino
+++ motors/motors.ino
@@ -7,7 +7,7 @@ void setup() {
   x_servo.attach(6);    // attaches the servo on pin 9 to the servo object
   y_servo.attach(9);    // attaches the servo on pin 9 to the servo object
   
-  Serial.begin(9600);   // begin serial communication at 9600 bits of data per second between the board and the computer
+  Serial.begin(115200);   // begin serial communication at 9600 bits of data per second between the board and the computer
 
   x_servo.write(90);    // sets the servo position according to the scaled value
   y_servo.write(10);    // sets the servo position according to the scaled value
@@ -15,11 +15,11 @@ void setup() {
 
 void loop() {
   while (Serial.available() > 0) {
-    int x_axis = Serial.parseFloat();   // convert from string to float number
-    int y_axis = Serial.parseFloat();   // convert from string to float number
+    float x_axis = Serial.parseFloat();   // convert from string to float number
+    float y_axis = Serial.parseFloat();   // convert from string to float number
     
-    Serial.print(x_axis);
-    Serial.println(y_axis);
+    //Serial.print(x_axis);
+    //Serial.println(y_axis);
 
     x_servo.write(x_axis);              // sets the servo position according to the scaled value
     y_servo.write(y_axis);              // sets the servo position according to the scaled value
